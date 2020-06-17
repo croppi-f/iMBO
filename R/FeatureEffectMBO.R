9ae012e9f5b197b10513f562262a4813b1ce7c23
FedericoCroppi <federico.croppi@gmail.com>
2020-06-17 13:40:12 +0200

    small update

--- R/FeatureEffectMBO.R
+++ R/FeatureEffectMBO.R
@@ -150,7 +150,7 @@ FeatureEffectMBO = function(res.mbo,
         attributes = FALSE
       )
       df = cbind(seen.points[[x]], ic)
-      df = data.table::data.table(df)
+      df = data.table::data.table(df, stringsAsFactors = TRUE)
       data.table::setnames(df, "ic", infill.mbo)
       df = as.data.frame(df)
     })
